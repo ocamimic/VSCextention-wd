c3ba917e01543e58321e0ba9b5eece47095eba14
ocami <66419581+ocamimic@users.noreply.github.com>
2025-02-06 22:44:18 +0900

    Create extention.ts
    進捗

---
+++ extention.ts
@@ -0,0 +1,134 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	const syntaxMainArray = ['size','toc','ul','li','ol','collapsible','a','image','gallery','note','html','code','table','row','hcell','cell','div','span','math','eref','footnote','footnoteblock','bibliography','date','include','embedvideo','embedaudio','embed','iframe','iftags','file','user','social','button','tabview','tab'];
+    const syntaxSubArray = ['class','id','x-small','xx-small','small','smaller','large','larger','x-large','xx-large','show','hide','style','type','hideLocation','top','both','bottom','link','alt','title','name','caption','width','align','right','left','center','clear','true','false','order','viewer','yes','no','desc',];
+
+    // wikidot構文で[[WORDS]]のように現れる単語を登録 
+	const syntaxWord = vscode.languages.registerCompletionItemProvider('wikidot', {
+		provideCompletionItems(_document: vscode.TextDocument, _position: vscode.Position, _token: vscode.CancellationToken, _context: vscode.CompletionContext) {
+			var syntaxWordArray = syntaxMainArray.concat(syntaxSubArray);
+			var syntaxWord = syntaxWordArray.map(syntax => new vscode.CompletionItem(syntax, vscode.CompletionItemKind.Text));
+
+                        return syntaxWord;
+                    }
+                }
+        );
+	//autoclose tags
+	const modules = [
+		//[[module name]], if close is true, insert[[/module]], args is arguments
+		{name: "ListDrafts", close: false},
+		{name: "ListPages", close: true},
+		{name: "CountPages", close: true},
+		{name: "ListUsers", close: true},
+		{name: "TagCloud", close: false},
+		{name: "PageCalendar", close: false},
+		{name: "PageTree", close: false},
+		{name: "Backlinks", close: false},
+		{name: "WantedPages", close: false},
+		{name: "OrphanedPages", close: false},
+		{name: "Categories", close: false},
+		{name: "Watchers", close: true},
+		{name: "Members", close: true},
+		{name: "Join", close: true},
+		{name: "SendInvitations", close: true},
+		{name: "WhoInvited", close: true},
+		{name: "CSS", close: true},
+		{name: "NewPage", close: true},
+		{name: "Clone", close: true},
+		{name: "Redirect", close: true},
+		{name: "ThemePreviewer", close: true},
+		{name: "MailForm", close: true},
+		{name: "PetitionAdmin", close: true},
+		{name: "SiteGrid", close: true},
+		{name: "FeaturedSite", close: true},
+		{name: "Feed", close: true},
+		{name: "FrontForum", close: true},
+		{name: "Comments", close: true},
+		{name: "RecentPosts", close: true},
+		{name: "MiniRecentThreads", close: true},
+		{name: "MiniActiveThreads", close: true},
+		{name: "MiniRecentPosts", close: true},
+		{name: "Rate", close: true},
+		{name: "RatedPages", close: true},
+		{name: "Gallery", close: true},
+		{name: "FlickrGallery", close: false},
+		{name: "Files", close: false},
+		{name: "Search", close: false},
+		{name: "SearchAll", close: false},
+		{name: "SearchUsers", close: false},
+		{name: "SiteChanges", close: false}
+	]
+	const moduleCompletion = vscode.languages.registerCompletionItemProvider('wikidot', {
+		provideCompletionItems(_document: vscode.TextDocument, _position: vscode.Position, _token: vscode.CancellationToken, _context: vscode.CompletionContext) {
+			return modules.map(module => {
+				const item = new vscode.CompletionItem(`[[module ${module.name}]]`, vscode.CompletionItemKind.Snippet);
+				if(module.close) {
+					item.insertText = new vscode.SnippetString(`module ${module.name}]]\n$0\n[[/module`);
+				}else {
+					item.insertText = new vscode.SnippetString(`module ${module.name}`);
+				}
+				return item
+			}
+		)
+	}});
+	const autoCloseTarget = [{name: 'size', inline: true, args: true, argsContent: [{name: '', value: ['xx-small','x-small','smaller','small', 'large','larger','x-large','xx-large','custom']}]},
+	{name: 'ul', inline: true,args: false},
+	{name: 'li', inline: true,args: false},
+	{name: 'ol', inline: true,args: false},
+	{name: 'collapsible',inline: false, args: true, argsContent: [{name: 'show', value: ['"+ open"']},{name: "hide", value: ['"- hide"']},{name: "hideLocation", value: ['"top"','"both"','"bottom"']}]},
+	{name: 'a', inline: true, args: true, argsContent: [{name: 'href', value: ['"http://scp-jp.wikidot.com/example.link"']}]},
+	{name: 'gallery', inline: false, args: true, argsContent: [{name: 'size', value: ['"square"', '"thumbnail"', '"small"', '"medium"']}]},
+	{name: 'note', inline: true,args: false},
+	{name: 'html', inline: false,args: false},
+	{name: 'code', inline: false, args: true, argsContent: [{name: 'type', value: []}]},
+	{name: 'table', inline: false, args: false},
+	{name: 'row', inline: false, args: false},
+	{name: 'hcell', inline: false, args: false},
+	{name: 'cell', inline: false, args: false},
+	{name: 'div', inline: false, args: false},
+	{name: 'span', inline: true, args: false},
+	{name: 'math', inline: true, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'footnote', inline: true, args: false},
+	{name: 'bibliography', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'embedvideo', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'embedaudio', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'embed', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'iframe', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'iftags', inline: false, args: true, argsContent: [{name: '', value: []}]},
+	{name: 'tabview', inline: false, args: false},
+	{name: 'tab', inline: false, args: true, argsContent: [{name: '', value: ['tab-Name']}]},
+	];
+	const autoCloseTags = vscode.languages.registerCompletionItemProvider('wikidot', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			return autoCloseTarget.map(tag => {
+				const cursor = tag.inline ? '$0' : '\n$0\n';
+				//argsがtrueのとき、argsContent.name=argsContent.valueとする
+				let snipArgs = '';
+				if(tag.args) {
+					const snipArgsArray: string[] = [];
+					tag.argsContent?.forEach((synArg, index) =>{
+						var options = '${' + (index + 1) + '|' + synArg.value.join(',') + '|}';
+						if(synArg.name != ''){
+							snipArgsArray.push(synArg.name + '=' + options);
+						}else{
+							snipArgsArray.push(options);
+						}});
+
+					snipArgs = ' ' + snipArgsArray.join(' ');
+					}
+					const BracketCount = (document.lineAt(position.line).text.substring(0, position.character).match(/\[{2}/g) || []).length;
+					let leftBrackets = '[[';
+					let rightBrackets = ']]';
+					if(BracketCount >= 1){
+						leftBrackets = '';
+						rightBrackets = '';
+				}
+				const item = new vscode.CompletionItem(`[[${tag.name}]]`, vscode.CompletionItemKind.Snippet);
+				item.insertText = new vscode.SnippetString(`${leftBrackets}${tag.name}${snipArgs}]]${cursor}[[/${tag.name}${rightBrackets}`);
+				return item;
+			}
+		)        
+	}});
+	context.subscriptions.push(syntaxWord, moduleCompletion, autoCloseTags);
+}
